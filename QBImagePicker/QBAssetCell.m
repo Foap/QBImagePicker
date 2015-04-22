daaded146843b326bf1254c31484aab50919f9fd
Knut Inge Grosland <hei@knutinge.com>
2015-04-22 13:36:24 +0100

    Update image loading from pod asset bundle

--- QBImagePicker/QBAssetCell.m
+++ QBImagePicker/QBAssetCell.m
@@ -10,11 +10,27 @@
 
 @interface QBAssetCell ()
 
-@property (weak, nonatomic) IBOutlet UIView *overlayView;
+@property (weak, nonatomic) IBOutlet UIImageView *overlayView;
 
 @end
 
 @implementation QBAssetCell
+
+- (void)awakeFromNib {
+
+    // Get asset bundle
+    static NSBundle *assetBundle = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        assetBundle = [NSBundle bundleForClass:[self class]];
+        NSString *bundlePath = [assetBundle pathForResource:@"QBImagePicker" ofType:@"bundle"];
+        if (bundlePath) {
+            assetBundle = [NSBundle bundleWithPath:bundlePath];
+        }
+    });
+    
+    self.overlayView.image = [UIImage imageNamed:@"selectedImage" inBundle:assetBundle compatibleWithTraitCollection:nil];
+}
 
 - (void)setSelected:(BOOL)selected
 {
